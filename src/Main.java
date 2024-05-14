80d0ab0f8f4ed10d2d25f2bcbc2c5d5c0f5d4b17
Ripfumelo2002 <ripfumelo.433@gmail.com>
2024-05-14 19:15:36 +0200

    First Changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = input.next() ;
+        System.out.println("Your name is: " + name);
+        System.out.println("Enter your age ");
+        int age = input.nextInt();
+        System.out.println("You are " + age + " years old");
+
     }
 }
